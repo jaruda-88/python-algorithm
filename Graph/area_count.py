3bff5d30da0df4aa7eca207b591f8b5761cf4fb3
jaruda-88 <jaruda88@gmail.com>
2023-04-04 20:53:37 +0900

    update

--- Graph/area_count.py
+++ Graph/area_count.py
@@ -2,32 +2,50 @@
 블럭을 쌓는데 블럭의 인접한 몉의 수가 가장 많은 중요 블럭의 좌표를 구함
 '''
 
-input = [["3 1 2"], ["2 2 2"], ["1 3 2"]]
+input = [["3"], ["1 1 1"]]
 
 maxCount = 0
-prevCount = 0
-areas = []
-width = len(input[0]) - 1
+areas = {}
 height = len(input) - 1
-
-
-def check(col_idx:int, row_idx:int):
-    result = 2
-
-    if col_idx == 0 and row_idx == 0 or row_idx == width:
-        return result
-    elif col_idx == height and row_idx == 0 or row_idx == width:
-        return result
-    elif col_idx > 0 and col_idx < height:
-        return result + 2
-    else:
-        return result + 1
+lengths = []
 
 for idx1, val1 in enumerate(input):
-    
-    count = 0
     row = ''.join(val1).replace(' ', '')
-    for idx2 in range(len(row)):
-        v1 = check(idx1, idx2)
-        print(v1)
+    lengRow = len(row)
+    lengths.append(lengRow)
+
+def check(width: int, col_idx:int, row_idx:int):
+    global maxCount
+    result = 0
+
+    if width == 0:
+        prevCol = col_idx - 1
+        nextCol = col_idx + 1
+        if prevCol >= 0:
+            result += lengths[prevCol]
+        if nextCol < height:
+            result += lengths[nextCol]
+        return
+
+    if col_idx != 0 and col_idx < height and row_idx > 0 and row_idx < width:
+        result = 4
+    else:
+        result = 3
     
+    if row_idx == 0 or row_idx == width:
+        result -= 1
+
+    if maxCount <= result:
+        maxCount = result
+
+        if maxCount not in areas:
+            areas[maxCount] = [(row_idx, col_idx)]
+        else:
+            areas[maxCount].append([row_idx, col_idx])
+
+for i in range(len(lengths)):
+    for j in range(lengths[i]):
+        check(lengths[i] - 1, i, j)
+    
+print(areas[max(areas)])
+print(areas)
